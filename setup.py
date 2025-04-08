2eca370ace14190d324ab224c4f99967ed9d7153
Matt Newsted <mnewsted@illinoislegalaid.org>
2025-04-08 09:24:42 -0500

    updated after internal review

--- setup.py
+++ setup.py
@@ -44,7 +44,7 @@ def find_package_data(where='.', package='', exclude=standard_exclude, exclude_d
     return out
 
 setup(name='docassemble.ParentingPlan',
-      version='0.8.14',
+      version='0.8.15',
       description=('A docassemble extension.'),
       long_description='# docassemble.ParentingPlan\r\n\r\nA docassemble extension.\r\n\r\n## Author\r\n\r\nMatt Newsted, mnewsted@illinoislegalaid.org\r\n\r\n',
       long_description_content_type='text/markdown',
