d3b71e58a902dd43bdf25e02d963a784df4f527b
Matt Newsted <mnewsted@illinoislegalaid.org>
2025-05-22 18:11:10 -0500

    most qa feedback implemented

--- setup.py
+++ setup.py
@@ -44,7 +44,7 @@ def find_package_data(where='.', package='', exclude=standard_exclude, exclude_d
     return out
 
 setup(name='docassemble.ParentingPlan',
-      version='0.8.18',
+      version='0.8.19',
       description=('A docassemble extension.'),
       long_description='# docassemble.ParentingPlan\r\n\r\nA docassemble extension.\r\n\r\n## Author\r\n\r\nMatt Newsted, mnewsted@illinoislegalaid.org\r\n\r\n',
       long_description_content_type='text/markdown',
@@ -53,7 +53,7 @@ setup(name='docassemble.ParentingPlan',
       license='The MIT License (MIT)',
       url='https://docassemble.org',
       packages=find_namespace_packages(),
-      install_requires=['docassemble.AssemblyLine @ git+https://github.com/SuffolkLITLab/docassemble-AssemblyLine.git@main', 'docassemble.ILAO @ git+https://github.com/IllinoisLegalAidOnline/docassemble-ILAO.git@main'],
+      install_requires=['docassemble.AssemblyLine @ git+https://github.com/SuffolkLITLab/docassemble-AssemblyLine.git@main', 'docassemble.ILAO>=1.1.10'],
       zip_safe=False,
       package_data=find_package_data(where='docassemble/ParentingPlan/', package='docassemble.ParentingPlan'),
      )
